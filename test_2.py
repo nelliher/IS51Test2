b9689f975a9b38b3c2807f20b2899c17bbd57af9
nelli her <nellih1117@gmail.com>
2021-04-29 18:17:51 -0700

    finish exam

--- test_2.py
+++ test_2.py
@@ -23,5 +23,19 @@ add sum of all grades / total number of grades
 # function3 
 n*100 / len(grades)
 
-#main
+main
 """
+infile = open("Final.txt" , 'r')
+grades = [line.rstrip() for line in infile]
+infile.close()
+for i in range(len(grades)):
+    grades[i] = int(grades[i])
+average = sum(grades) / len(grades)
+num = 0
+for grade in grades:
+    if grade > average:
+        num += 1
+print("Number of grades:" , len(grades))
+print("Average grade:" , average)
+print("Percentage of grades above average: {0:.2f}%"
+                    .format(100 * num / len(grades)))
