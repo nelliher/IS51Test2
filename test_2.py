e9887078a94791bbc4bcac46cc3e303d5bb5cd5a
nelli her <nellih1117@gmail.com>
2021-04-29 17:56:53 -0700

    finish structured english

---
+++ test_2.py
@@ -0,0 +1,14 @@
+
+"""
+This program will display the class exam averages based on the total number of grades. The first calculation will display the number of grades.
+The second calculation will display the average of the total grades. The third calculation will display the total percentage of the grades abover average. 
+There will be three functions. The first function will initialize the apllication. The second function will calcualte the total percentage above average. 
+The third function will calculate the percentage of total grades above average. 
+
+Function 1 will initialize the application.
+Function 2 will sum all the total number of grades.
+Function 3 will multiply all of the grades by 100 and divide it by the total number of grades. 
+
+It will output the percentage above average. And after the code has ran, it will output the percentage of the total grades above average. 
+
+"""
